1a53d2afeb60f342c510c5da6a36d5a0fdbf5678
Tomoki Umezawa <tomoki.umezawa@jp.ricoh.com>
2021-01-20 18:52:27 +0900

    For test

---
+++ python/source/b.py
@@ -0,0 +1,14 @@
+# -*-coding: utf-8 -*-
+
+a = 100000000000
+a = (
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    + "ssssssssssssssssssssssssssssssssssssssssssss"
+    + "testtest"
+)
+
+
+def b_test_format(
+    aaaaaaaaaaaaa, bbbbbbbbbbbbbbbb, cccccccccccccccc, dddddddddddddddddd, eeeeeeeeeeeeeeeeee
+):
+    print("hoge")
