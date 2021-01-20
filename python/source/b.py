502a0786f888d64d26c443f977b036eb54ae6ab6
tumezawa <tomoki.umezawa@jp.ricoh.com>
2021-01-20 19:04:41 +0900

    Update b.py
    test b.py

--- python/source/b.py
+++ python/source/b.py
@@ -1,8 +1,11 @@
 # -*-coding: utf-8 -*-
 
 a = 100000000000
-b = ("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+"ssssssssssssssssssssssssssssssssssssssssssss"+"testtest")
-
+b = ("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+"ssssssssssssssssssssssssssssssssssssssssssss"+"testtest")       
 
 def b_test_format(aaaaaaaaaaaaa, bbbbbbbbbbbbbbbb, cccccccccccccccc, dddddddddddddddddd, eeeeeeeeeeeeeeeeee):
     print("hoge")
+
+    
+    
+    
